aeb0a1364675f6f78862d8e640dbb83273cad3fa
FK1453 <barka_21@hotmail.com>
2016-07-16 12:40:27 +0300

    Added Database Class

--- app/Config.php
+++ app/Config.php
@@ -2,16 +2,26 @@
     return array(
 
 
-        
         //App Variables
         "app"      => array(
-            "layout"   => "Layout/Default",
-            "base_url" => NULL,
-            "handle_errors" => TRUE,
-            "log_errors" => FALSE,
+            "layout"                => "Layout/Default",
+            "base_url"              => NULL,
+            "handle_errors"         => TRUE,
+            "log_errors"            => FALSE,
             "router.case_sensitive" => TRUE
         ),
 
+
+        //Database Variables
+        "database" => array(
+            //mysql, sqlsrv, pgsql are tested connections and work perfect.
+            "connection" => "mysql",
+            "host"       => "127.0.0.1",
+            "port"       => "3306",
+            "name"       => "databasename",
+            "user"       => "username",
+            "password"   => "password"
+        ),
 
 
         //Routes
