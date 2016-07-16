6a4b7470f92a8630a77573e242f6ad3ba8569440
FK1453 <barka_21@hotmail.com>
2016-07-16 12:49:20 +0300

    Added Database Class

---
+++ src/Wow/Database/Database.php
@@ -0,0 +1,348 @@
+<?php
+
+    namespace Wow\Database;
+
+    use PDO;
+    use PDOException;
+    use PDOStatement;
+    use Wow;
+
+    /**
+     * Class Database
+     * @package Wow\Database
+     * Thanks indieteq for class, see the original: https://github.com/indieteq/indieteq-php-my-sql-pdo-database-class
+     */
+    class Database {
+        /**
+         * @var $pdo PDO
+         */
+        private $pdo;
+
+        /**
+         * @var $sQuery PDOStatement
+         */
+        private $sQuery;
+
+        /**
+         * @var array $settings Settings
+         */
+        private $settings;
+
+        /**
+         * @var bool $bConnected Is Datadabase Connected
+         */
+        private $bConnected = FALSE;
+
+
+        /**
+         * @var array $parameters
+         */
+        private $parameters;
+
+        /**
+         *   Default Constructor
+         *
+         *    1. Connect to database.
+         *    2. Creates the parameter array.
+         */
+        public function __construct() {
+            $this->Connect();
+            $this->parameters = array();
+        }
+
+        /**
+         *    This method makes connection to the database.
+         *
+         *    1. Reads the database settings from a ini file.
+         *    2. Puts  the ini content into the settings array.
+         *    3. Tries to connect to the database.
+         *    4. If connection failed, exception is displayed and a log file gets created.
+         */
+        private function Connect() {
+            // Get database settings from Config File
+            $this->settings = array(
+                "connection" => Wow::get("database.connection"),
+                "host"       => Wow::get("database.host"),
+                "port"       => Wow::get("database.port"),
+                "name"       => Wow::get("database.name"),
+                "user"       => Wow::get("database.user"),
+                "password"   => Wow::get("database.password")
+            );
+
+            try {
+                switch($this->settings["connection"]) {
+                    case "mysql":
+                        $dsn = "mysql:host=" . $this->settings["host"];
+                        if(empty($this->settings["port"])) {
+                            $dsn .= "; port=" . $this->settings["port"];
+                        }
+                        $dsn .= "; dbname=" . $this->settings["name"];
+                        break;
+                    case "sqlsrv":
+                        $dsn = "sqlsrv:server=" . $this->settings["host"];
+                        if(empty($this->settings["port"])) {
+                            $dsn .= "; port=" . $this->settings["port"];
+                        }
+                        $dsn .= "; Database=" . $this->settings["name"];
+                        break;
+                    case "pgsql":
+                        $dsn = "pgsql:host=" . $this->settings["host"];
+                        if(empty($this->settings["port"])) {
+                            $dsn .= "; port=" . $this->settings["port"];
+                        }
+                        $dsn .= "; dbname=" . $this->settings["name"];
+                        break;
+                    default:
+                        $dsn = $this->settings["connection"] . ":host=" . $this->settings["host"];
+                        if(empty($this->settings["port"])) {
+                            $dsn .= "; port=" . $this->settings["port"];
+                        }
+                        $dsn .= "; dbname=" . $this->settings["name"];
+                        break;
+                }
+                $this->pdo = new PDO($dsn, $this->settings["user"], $this->settings["password"], array(
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+                ));
+
+                /**
+                 * We can now track any exceptions on Fatal error.
+                 */
+                $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                /**
+                 * Disable emulation of prepared statements, use REAL prepared statements instead.
+                 */
+                $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
+
+                /**
+                 * Connection succeeded, set the boolean to true.
+                 */
+                $this->bConnected = TRUE;
+            } catch(PDOException $e) {
+                die("Error in Database Connection. Could not connect to Database!");
+            }
+        }
+
+        /**
+         *   You can use this little method if you want to close the PDO connection
+         *
+         */
+        public function CloseConnection() {
+            /**
+             * Set the PDO object to null to close the connection
+             * http://www.php.net/manual/en/pdo.connections.php
+             */
+            $this->pdo = NULL;
+        }
+
+        /**
+         *    Every method which needs to execute a SQL query uses this method.
+         *
+         *    1. If not connected, connect to the database.
+         *    2. Prepare Query.
+         *    3. Parameterize Query.
+         *    4. Execute Query.
+         *    5. On exception : Write Exception into the log + SQL query.
+         *    6. Reset the Parameters.
+         */
+        private function Init($query, $parameters = "") {
+            // Connect to database
+            if(!$this->bConnected) {
+                $this->Connect();
+            }
+            try {
+                // Prepare query
+                $queryOptions = array();
+                if($this->settings["connection"] == "sqlsrv") {
+                    $queryOptions = array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL);
+                }
+                $this->sQuery = $this->pdo->prepare($query, $queryOptions);
+
+                // Add parameters to the parameter array
+                $this->bindMore($parameters);
+
+                // Bind parameters
+                if(!empty($this->parameters)) {
+                    foreach($this->parameters as $param => $value) {
+
+                        $type = PDO::PARAM_STR;
+                        switch($value[1]) {
+                            case is_int($value[1]):
+                                $type = PDO::PARAM_INT;
+                                break;
+                            case is_bool($value[1]):
+                                $type = PDO::PARAM_BOOL;
+                                break;
+                            case is_null($value[1]):
+                                $type = PDO::PARAM_NULL;
+                                break;
+                        }
+                        // Add type when binding the values to the column
+                        $this->sQuery->bindValue($value[0], $value[1], $type);
+                    }
+                }
+
+                /**
+                 * Execute SQL
+                 */
+                $this->sQuery->execute();
+            } catch(PDOException $e) {
+                /**
+                 * Display Exception
+                 */
+                die("Error in Sql Query. Query executation failed! Please check your sql query.");
+            }
+
+            /**
+             * Reset the parameters
+             */
+            $this->parameters = array();
+        }
+
+        /**
+         * @void
+         *
+         *    Add the parameter to the parameter array
+         *
+         * @param string $para
+         * @param string $value
+         */
+        public function bind($para, $value) {
+            $this->parameters[sizeof($this->parameters)] = [
+                ":" . $para,
+                $value
+            ];
+        }
+
+        /**
+         * @void
+         *
+         *    Add more parameters to the parameter array
+         *
+         * @param array $parray
+         */
+        public function bindMore($parray) {
+            if(empty($this->parameters) && is_array($parray)) {
+                $columns = array_keys($parray);
+                foreach($columns as $i => &$column) {
+                    $this->bind($column, $parray[$column]);
+                }
+            }
+        }
+
+        /**
+         *  If the SQL query  contains a SELECT or SHOW statement it returns an array containing all of the result set row
+         *    If the SQL statement is a DELETE, INSERT, or UPDATE statement it returns the number of affected rows
+         *
+         * @param  string $query
+         * @param  array  $params
+         * @param  int    $fetchmode
+         *
+         * @return mixed
+         */
+        public function query($query, $params = NULL, $fetchmode = PDO::FETCH_ASSOC) {
+            $query = trim(str_replace("\r", " ", $query));
+
+            $this->Init($query, $params);
+
+            $rawStatement = explode(" ", preg_replace("/\s+|\t+|\n+/", " ", $query));
+
+            /**
+             * Which SQL statement is used
+             */
+            $statement = strtolower($rawStatement[0]);
+
+            if($statement === 'select' || $statement === 'show') {
+                return $this->sQuery->fetchAll($fetchmode);
+            } elseif($statement === 'insert' || $statement === 'update' || $statement === 'delete') {
+                return $this->sQuery->rowCount();
+            } else {
+                return NULL;
+            }
+        }
+
+        /**
+         *  Returns the last inserted id.
+         * @return string
+         */
+        public function lastInsertId() {
+            return $this->pdo->lastInsertId();
+        }
+
+        /**
+         * Starts the transaction
+         * @return boolean, true on success or false on failure
+         */
+        public function beginTransaction() {
+            return $this->pdo->beginTransaction();
+        }
+
+        /**
+         *  Execute Transaction
+         * @return boolean, true on success or false on failure
+         */
+        public function executeTransaction() {
+            return $this->pdo->commit();
+        }
+
+        /**
+         *  Rollback of Transaction
+         * @return boolean, true on success or false on failure
+         */
+        public function rollBack() {
+            return $this->pdo->rollBack();
+        }
+
+        /**
+         *    Returns an array which represents a column from the result set
+         *
+         * @param  string $query
+         * @param  array  $params
+         *
+         * @return array
+         */
+        public function column($query, $params = NULL) {
+            $this->Init($query, $params);
+            $Columns = $this->sQuery->fetchAll(PDO::FETCH_NUM);
+
+            $column = NULL;
+
+            foreach($Columns as $cells) {
+                $column[] = $cells[0];
+            }
+
+            return $column;
+
+        }
+
+        /**
+         *    Returns an array which represents a row from the result set
+         *
+         * @param  string $query
+         * @param  array  $params
+         * @param  int    $fetchmode
+         *
+         * @return array
+         */
+        public function row($query, $params = NULL, $fetchmode = PDO::FETCH_ASSOC) {
+            $this->Init($query, $params);
+            $result = $this->sQuery->fetch($fetchmode);
+            $this->sQuery->closeCursor(); // Frees up the connection to the server so that other SQL statements may be issued,
+            return $result;
+        }
+
+        /**
+         *    Returns the value of one single field/column
+         *
+         * @param  string $query
+         * @param  array  $params
+         *
+         * @return string
+         */
+        public function single($query, $params = NULL) {
+            $this->Init($query, $params);
+            $result = $this->sQuery->fetchColumn();
+            $this->sQuery->closeCursor(); // Frees up the connection to the server so that other SQL statements may be issued
+            return $result;
+        }
+    }
