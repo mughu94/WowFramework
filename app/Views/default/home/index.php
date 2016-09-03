4f47cc3ce1d26c5eb40a24794f5b009a08224f87
FK1453 <barka_21@hotmail.com>
2016-09-03 16:53:42 +0300

    Rework

--- app/Views/default/home/index.php
+++ app/Views/default/home/index.php
@@ -1,6 +1,9 @@
 <?php
     /**
      * @var \Wow\Template\View $this
+     * @var mixed $model
      */
 ?>
+<div class="container">
     <p>A new wow application is baking.</p>
+</div>
--- app/Views/default/layout/default.php
+++ app/Views/default/layout/default.php
@@ -11,23 +11,25 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/assets/bootstrap/3.3.6/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/assets/bootstrap/3.3.6/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="/assets/style/style.css">
     <title><?php if($this->has('title')) {
             echo $this->get("title") . " | ";
         } ?>Wow</title>
     <?php if($this->has('description')) { ?>
-    <meta name="description" content="<?php echo $this->get("description"); ?>"><?php } ?>
+        <meta name="description" content="<?php echo $this->get("description"); ?>"><?php } ?>
     <?php if($this->has('keywords')) { ?>
-    <meta name="keywords" content="<?php echo $this->get("keywords"); ?>"><?php } ?>
+        <meta name="keywords" content="<?php echo $this->get("keywords"); ?>"><?php } ?>
+    <?php $this->section('section_head'); ?>
+    <link rel="stylesheet" href="/assets/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/bootstrap/3.3.6/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="/assets/style/style.css">
+    <?php $this->show() ?>
 </head>
 <body>
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -37,15 +39,15 @@
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <p class="navbar-text navbar-right">
-              Wellcome.
+                Wellcome.
             </p>
         </div>
     </div>
 </nav>
-<div class="container">
-    <?php $this->renderSection('section_pageheader'); ?>
-    <?php $this->renderBody(); ?>
-</div>
+<?php
+    $this->renderSection('section_pageheader');
+    $this->renderBody();
+?>
 <?php $this->section('section_scripts'); ?>
 <script src="/assets/jquery/2.2.4/jquery.min.js"></script>
 <script src="/assets/bootstrap/3.3.6/js/bootstrap.min.js"></script>
